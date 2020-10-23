4938b334f15178b7b3cfcc64a6735bea8d32df93
Loc Nguyen <vinhloc30796@gmail.com>
2020-10-23 17:21:31 +0700

    Clean up structure; Add calc_normalized_returns to StockUniverse

--- download_data.py
+++ download_data.py
@@ -23,6 +23,14 @@ def get_sp500_tickers(filename='../data/sp500_tickers.txt'):
 
 class StockUniverse:
     def __init__(self):
+        self.indicator_translation = {
+            'A': 'Adj Close',
+            'O': 'Open',
+            'H': 'High',
+            'L': 'Low',
+            'C': 'Close',
+            'V': 'Volume'
+        }
         return None
     
     def download_data(self, tickers, start="2017-01-01", end="2020-10-23"):
@@ -50,22 +58,14 @@ class StockUniverse:
         :param filename: str e.g. 'pricing.pkl'
         :param indicator_select: list of str - which indicator(s) to save in the csv
             Possible strings are:
-            A: adj close; O: open; C: close;
-            H: high; L: low, V: volume
+            A: Adj Close; O: Open; C: Close;
+            H: High; L: Low, V: Volume
         :return filename
         """
         self.filename = filename
         # Select indicator(s) to save in the csv
-        indicator_translation = {
-            'A': 'Adj Close',
-            'O': 'Open',
-            'H': 'High',
-            'L': 'Low',
-            'C': 'Close',
-            'V': 'Volume'
-        }
         indicator_columns = [
-            indicator_translation[each_selection] 
+            self.indicator_translation[each_selection] 
             for each_selection in indicator_select
         ]
         df_to_save = self.df.loc[:, indicator_columns]
@@ -92,6 +92,48 @@ class StockUniverse:
             self.tickers = self.df.columns.get_level_values(1) # Assumes that self.df.columns is MultiIndex
             self.start, self.end = self.df.index[[0,-1]] # Get start and end time
         return self.df
+
+    def calc_normalized_returns(
+        self,
+        indicator_select=['A','O','H','L','C','V']
+    ):
+        """Read a local pickle file into self.df
+        
+        :param :param indicator_select: list of str - which indicator(s) 
+            to calculate returns for.
+            Possible strings are:
+            A: Adj Close; O: Open; C: Close;
+            H: High; L: Low, V: Volume
+        :return self.df with the normalized returns
+        accessible through e.g. self.df['Adj Close Returns']
+        """
+        # Select indicator(s) to save in the csv
+        indicator_columns = [
+            self.indicator_translation[each_selection] 
+            for each_selection in indicator_select
+        ]
+        # Filter self.df on indicator_columns
+        df_to_calc = self.df.loc[:, indicator_columns]
+        # Calculate the returns
+        returns_df = (df_to_calc - df_to_calc.shift())/(df_to_calc.shift())
+        # Build new index and column names
+        indicator_names = [
+            each_indicator_name + ' Returns'
+            for each_indicator_name 
+            in df_to_calc.columns.get_level_values(0).unique()
+        ]
+        ticker_names = df_to_calc.columns.get_level_values(1).unique()
+        index_names = df_to_calc.columns.names
+        # Store returns_df in StockUniverse
+        self.returns_df = pd.DataFrame(
+            data=returns_df.values,
+            index=df_to_calc.index,
+            columns=pd.MultiIndex.from_product(
+                [indicator_names, ticker_names],
+                names=index_names
+            )
+        )
+        return self.returns_df
 
 if __name__ == "__main__":
     # Main
